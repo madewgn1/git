1791fa5a976e82dda0699c27e765249b11fe1e8f
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-30 18:53:44 +0800

    update

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ def get_pw(username):
     return None
  
 
+@app.after_request
+def add_header(r):
+
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 
 app = Flask(__name__)
 logging.config.fileConfig('logging.ini')    # by logging config file.
